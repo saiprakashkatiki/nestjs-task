65c4d27dfa7e4fa279dd5a5c083b10b0c2a71e34
saiprakashkatiki <saiprakash@schemaxtech.com>
2022-02-02 21:16:10 +0530

    final commit

--- src/plants/plant.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Plant } from './entities/plant.entity'; 
-
-@EntityRepository(Plant)
-export class PlantRepository extends Repository<Plant> {}
